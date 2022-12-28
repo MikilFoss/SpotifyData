87ffa88e3ff3034949a0d49c1a3e631a77632ecf
Owen Ratgen <ratge006@umn.edu>
2022-12-27 18:05:31 -0600

    Was able to get login button and have the user be able to login to their spotify account

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-/* Load the HTTP library */
-var http = require('http');
+var express = require('express');
+var request = require('request');
+var querystring = require('querystring');
+const app = express();
+const port = 3000;
 
-/* Create an HTTP server to handle responses */
+var client_id = 'c4f0958cbf0741fcaa7dc824e1aca38a'; // Your client id
+var client_secret = 'edd99fffb1ea4007a1e1764060544774'; // Your secret
+var redirect_uri = 'http://localhost:3000/callback';
+  
+// Send homepage to the user
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-http
-  .createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
-  })
-  .listen(8888);
+// Have the user login to their spotify account
+app.get('/login', function(req, res) {
+
+    var scope = 'user-read-private user-read-email';
+    // your application requests authorization
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri
+      }));
+  });
+
+
+// Establishes connection between code and web server
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
